501b63ff5f75c3e6bea6f395212b73fe48e63cf8
Justus Renton <jrenton@sittercity.com>
2021-11-27 14:00:25 -0600

    first commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import App from './App.vue';
+import GmapVue from 'gmap-vue'
+
+Vue.use(GmapVue, {
+  load: {
+    key: '',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+
+    //// If you want to set the version, you can do so:
+    // v: '3.26',
+  },
+})
+
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
